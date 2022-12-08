bfdad3fab6eb438a88424de4808fb8f3ca0cd16e
Lucas_oliveira <wannacry.bat@proton.me>
2022-12-08 08:59:55 -0300

    typo

--- dnsbrute.py
+++ dnsbrute.py
@@ -1,7 +1,9 @@
 import sys
 import dns.resolver
+#importando as livrarias
 
 resolver = dns.resolver.Resolver()
+#criando a função
 
 try:
 	alvo = sys.argv[1]
@@ -9,14 +11,16 @@ try:
 except:
 	print("usage: python3 dnsbrute.py dominio  wordlist.txt")
 	sys.exit()
-
+#aqui estamos dizendo para o usuario colocar 2 argumentos, alvo e wordlist, e caso tenha erro, ele volta o print mostrando um exemplo de uso
+	
 try:
 	with open(wordlist,"r") as arq:
 		subdominios = arq.read().splitlines()
 except:
 	print("Erro ao abrir arquivo")
 	sys.exit()
-
+#aqui pedimos para abrir o arquivo wordlist, e definimos o subdominio como 
+	
 for subdominio in subdominios:
 	try:
 		sub_alvo = "{}.{}".format(subdominio, alvo)
