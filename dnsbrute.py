8a16a58b592014f6e60d908704e85d98ced7b014
wannacry3030 <wannacry.bat@proton.me>
2023-06-17 23:56:28 -0300

    update dnsbrute.py

--- dnsbrute.py
+++ dnsbrute.py
@@ -1,31 +1,50 @@
 import sys
 import dns.resolver
-#importando as livrarias
 
 resolver = dns.resolver.Resolver()
-#criando a função
 
 try:
-	alvo = sys.argv[1]
-	wordlist = sys.argv[2]
-except:
-	print("usage: python3 dnsbrute.py dominio  wordlist.txt")
-	sys.exit()
-#aqui estamos dizendo para o usuario colocar 2 argumentos, alvo e wordlist, e caso tenha erro, ele volta o print mostrando um exemplo de uso
-	
+    alvo = sys.argv[1]
+    wordlist = sys.argv[2]
+except IndexError:
+    print("usage: python3 dnsbrute.py dominio wordlist.txt")
+    sys.exit()
+
 try:
-	with open(wordlist,"r") as arq:
-		subdominios = arq.read().splitlines()
-except:
-	print("Erro ao abrir arquivo")
-	sys.exit()
-#aqui pedimos para abrir o arquivo wordlist, e definimos o subdominio como 
-	
-for subdominio in subdominios:
-	try:
-		sub_alvo = "{}.{}".format(subdominio, alvo)
-		resultados = resolver.resolve(sub_alvo,"A")
-		for resultado  in resultados:
-			print("{} -> {}".format(sub_alvo, resultado))
-	except:
-		pass
+    with open(wordlist, "r") as arq:
+        subdominios = arq.read().splitlines()
+except FileNotFoundError:
+    print("Erro: Arquivo não encontrado")
+    sys.exit()
+except IOError:
+    print("Erro: Falha ao ler o arquivo")
+    sys.exit()
+
+total_subdominios = len(subdominios)
+print(f"Total de subdomínios a serem testados: {total_subdominios}")
+
+subdominios_encontrados = []
+
+for index, subdominio in enumerate(subdominios, 1):
+    try:
+        sub_alvo = f"{subdominio}.{alvo}"
+        resultados = resolver.resolve(sub_alvo, "A")
+        for resultado in resultados:
+            subdominios_encontrados.append(sub_alvo)
+    except dns.resolver.NXDOMAIN:
+        pass
+    except dns.resolver.NoAnswer:
+        pass
+    except dns.exception.DNSException:
+        pass
+
+    print(f"Progresso: {index}/{total_subdominios}")
+
+print("Subdomínios encontrados:")
+if subdominios_encontrados:
+    for subdominio_encontrado in subdominios_encontrados:
+        print(subdominio_encontrado)
+else:
+    print("Nenhum subdomínio encontrado.")
+
+print("Conclusão da execução do script.")
